8a33c64a71f3681f716075e9bb06b4e4780afd7f
Name-user <82359728+Studio-user@users.noreply.github.com>
2023-01-14 07:28:48 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const cors = require('cors');
 const { response } = require('express');
 const fs = require('fs');
 const path = require('path');
+const { encode } = require('punycode');
+const { resolve } = require('dns');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
@@ -15,28 +17,63 @@ app.use(express.urlencoded({
 }))
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send(`Hello. It is for downloading musics.`);
+})
+
+app.get('/title', (req, res) => {
+  const url = req.query.url;
+  let mp3Title = '';
+  const title = async function(url) {
+    await new Promise((resolve) => {
+      ytdl.getInfo(url)
+      .then(data => {
+        mp3Title = encodeURI(data.videoDetails.title);
+        res.send(JSON.stringify({
+          Title: mp3Title
+        }));
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      })
+    })
+  }
+  title(url)
 })
 
 app.get('/download', (req, res) => {
   const url = req.query.url;
   const path1 = __dirname + '/video.mp3';  
   const video = async function(url) {
-    
     await new Promise((resolve) => {
       ytdl(url, {filter:'audioonly'})
       .pipe(fs.createWriteStream('video.mp3'))
       .on('close', () => {
         res.sendFile(path1);
-        resolve('finish');
+        setTimeout(() => {
+          resolve();
+        }, 2000)
       })
     })
   }
   video(url).then(() => {
+    let OK = true;
     setTimeout(() => {
-      fs.rmSync(path1);
-    });
-  });
+      while(OK) {
+        console.log('aa')
+        if(fs.existsSync(path1)){
+          fs.rmSync(path1);
+          console.log('bb')
+          OK = false;
+          break;
+        }
+        break;
+      }
+    }, 2000)
+  })
+})
+
+app.get('*', (req, res) => {
+  res.send('What do you want?');
 })
 
 app.listen(3000)
